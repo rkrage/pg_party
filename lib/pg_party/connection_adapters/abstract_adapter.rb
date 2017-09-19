34cfde4b3c6784ad9e6b07d37634b4077da56762
Ryan Krage <krage.ryan@gmail.com>
2017-09-18 22:10:00 -0500

    fix code climate issues

--- lib/pg_party/connection_adapters/abstract_adapter.rb
+++ lib/pg_party/connection_adapters/abstract_adapter.rb
@@ -1,31 +1,31 @@
 module PgParty
   module ConnectionAdapters
     module AbstractAdapter
-      def create_range_partition(*args)
+      def create_range_partition(*)
         raise NotImplementedError, "#create_range_partition is not implemented"
       end
 
-      def create_list_partition(*args)
+      def create_list_partition(*)
         raise NotImplementedError, "#create_list_partition is not implemented"
       end
 
-      def create_range_partition_of(*args)
+      def create_range_partition_of(*)
         raise NotImplementedError, "#create_range_partition_of is not implemented"
       end
 
-      def create_list_partition_of(*args)
+      def create_list_partition_of(*)
         raise NotImplementedError, "#create_list_partition_of is not implemented"
       end
 
-      def attach_range_partition(*args)
+      def attach_range_partition(*)
         raise NotImplementedError, "#attach_range_partition is not implemented"
       end
 
-      def attach_list_partition(*args)
+      def attach_list_partition(*)
         raise NotImplementedError, "#attach_list_partition is not implemented"
       end
 
-      def detach_partition(*args)
+      def detach_partition(*)
         raise NotImplementedError, "#detach_partition is not implemented"
       end
     end
